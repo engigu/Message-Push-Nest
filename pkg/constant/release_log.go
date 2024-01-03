a93cf7c304e09448085abf1e59291055cabc52dd
engigu <sayheya@qq.com>
2024-01-03 20:04:03 +0800

    change version log

--- pkg/constant/release_log.go
+++ pkg/constant/release_log.go
@@ -1,13 +1,22 @@
 package constant
 
+import "strings"
+
 var LatestVersion = map[string]string{}
 
 var V1Version = "v0.0.1"
-var V1VersionDesc = `1. 支持邮件消息发送
-2. 支持日志定时删除
-3. 支持账号密码重置`
+var V1VersionDesc = `
+1. 单应用打包
+2. 支持邮件发送
+3. 用户密码设置
+4. 支持用户定时任务清理，更新定时时间
+5. 查看定时清理日志
+6. 单应用的html浏览器自动缓存
+7. gin的日志使用logrus
+8. 支持异步发送
+`
 
 func init() {
 	LatestVersion["version"] = V1Version
-	LatestVersion["desc"] = V1VersionDesc
+	LatestVersion["desc"] = strings.TrimSpace(V1VersionDesc)
 }
