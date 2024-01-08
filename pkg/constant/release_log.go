0726e6d26f391cdfd9a2471c20df02c3c34f00e7
engigu <sayheya@qq.com>
2024-01-08 19:50:24 +0800

    fix: login expire auto redirect

--- pkg/constant/release_log.go
+++ pkg/constant/release_log.go
@@ -43,7 +43,22 @@ var V3VersionDesc = `
 10. 支持自定义的webhook推送
 `
 
+var V4Version = "v0.0.4"
+var V4VersionDesc = `
+1. 单应用打包
+2. 支持邮件发送
+3. 用户密码设置
+4. 支持用户定时任务清理，更新定时时间
+5. 查看定时清理日志
+6. 单应用的html浏览器自动缓存
+7. gin的日志使用logrus
+8. 支持异步发送
+9. 支持钉钉消息推送
+10. 支持自定义的webhook推送
+11. 支持自动初始化表，以及初始化账号
+`
+
 func init() {
-	LatestVersion["version"] = V3Version
-	LatestVersion["desc"] = strings.TrimSpace(V3VersionDesc)
+	LatestVersion["version"] = V4Version
+	LatestVersion["desc"] = strings.TrimSpace(V4VersionDesc)
 }
