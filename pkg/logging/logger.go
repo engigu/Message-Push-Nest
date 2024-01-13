547aa933c5d6b0b51212a42f5a1c7b096039b226
engigu <sayheya@qq.com>
2024-01-13 12:55:47 +0800

    update log ms display

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -36,7 +36,7 @@ func Setup() {
 	formatter.ForceFormatting = true
 	formatter.FullTimestamp = true
 	formatter.CallerFormatter = CallerFormatter
-	formatter.TimestampFormat = "2006-01-02 15:04:05.999"
+	formatter.TimestampFormat = "2006-01-02 15:04:05.000"
 	formatter.SetColorScheme(&prefixed.ColorScheme{
 		TimestampStyle: "cyan",
 		CallerStyle:    "cyan",
